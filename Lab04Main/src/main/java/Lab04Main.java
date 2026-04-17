c4d65057a775eab1b87b7df5318a3043cedc689d
nawodyanimalarathna42 <nawodyanimalarathna42@gmail.com>
2026-04-17 11:44:01 +0200

    lab04 q3 second child class

--- Lab04Main/src/main/java/Lab04Main.java
+++ Lab04Main/src/main/java/Lab04Main.java
@@ -10,8 +10,14 @@
 public class Lab04Main {
     public static void main(String[] args) {
         Lecturer lecturer = new Lecturer("Amal perera", "L001", "Computer Science", 3, 50000.0);
-
+        LabAssistant assistant = new LabAssistant("Amali nayana", "A002", "IT Department", 40, 1500.0);
+        
+        System.out.println("--- Lecturer Details ---");
         lecturer.displayLecturerDetails();
         System.out.println("Monthly Payment: " + lecturer.calculateMonthlyPayment());
+        
+        System.out.println("\n--- Lab Assistant Details ---");
+        assistant.displayLabAssistantDetails();
+        System.out.println("Monthly Payment: " + assistant.calculateMonthlyPayment());
     }
-}
+    }
